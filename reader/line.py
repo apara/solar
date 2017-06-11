4f289543f36b94f1339d79dc5df6adf41258f01f
apara <apara@standardset.com>
2017-06-11 15:57:38 -0700

    Parsing line 130

--- reader/line.py
+++ reader/line.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 class LineId(str):
     def __init__(self, value: str = None) -> None:
         super().__init__(value)
@@ -6,10 +8,30 @@ class LineId(str):
 # Line base class
 #
 class Line:
-    def __init__(self, lid: LineId=None) -> None:
+    def __init__(self, lid: LineId=None, array: object = None) -> None:
         super().__init__()
+
+        # Assign id of the line
+        #
         self.id = lid
-                
+
+        # Assign type of the line
+        #
+        if array:
+            self.type = array[0]
+            self.ts = self.parse_date(array[1])
+
+    def __str__(self) -> str:
+        return \
+            super().__str__() + \
+            ' id: ' + (self.id if self.id else 'None') + \
+            ' type: ' + (self.type if self.type else 'None') + \
+            ' ts: ' + self.ts.__str__()
+
+    @staticmethod
+    def parse_date(value:str)->datetime:
+        return datetime.strptime(value, '%Y%m%d%H%M%S')
+
 
 emptyLine = Line()
 
@@ -18,14 +40,13 @@ emptyLine = Line()
 #
 class Line130(Line):
     def __init__(self, lid: object = None, array: object = None) -> None:
-        super().__init__(lid)
-        print(array)
+        super().__init__(lid, array)
 
 
 # Type 140
+#
 class Line140(Line):
-    def __init__(self, lid: LineId=None, array=None) -> None:
-        super().__init__(lid)
-        print(array)
+    def __init__(self, lid: LineId=None, array: object = None) -> None:
+        super().__init__(lid, array)
 
 
--- tests/test_line_builder.py
+++ tests/test_line_builder.py
@@ -3,26 +3,27 @@ import unittest
 from reader.line import *
 from reader.line_builder import LinesFactory, LineFactory, LineBuilder130, LineBuilder140
 
+__GLOBAL_DATE = '20170607165000'
 
 class TestLineBuilder130(unittest.TestCase):
 
     def test_build(self):
-        result = LineBuilder130().build(['130', 'foo'])
+        result = LineBuilder130().build(['130', '20170607165000'])
         self.assertIsNot(result, emptyLine, 'result must not be an emptyLine')
 
     def test_build_not(self):
-        result = LineBuilder130().build(['120', 'foo'])
+        result = LineBuilder130().build(['120', '20170607165000'])
         self.assertIs(result, emptyLine, 'result must be emptyLine')
 
 
 class TestLineBuilder140(unittest.TestCase):
 
     def test_build(self):
-        result = LineBuilder140().build(['140', 'foo'])
+        result = LineBuilder140().build(['140', '20170607165000'])
         self.assertIsNot(result, emptyLine, 'result must not be an emptyLine')
 
     def test_build_not(self):
-        result = LineBuilder140().build(['120', 'foo'])
+        result = LineBuilder140().build(['120', '20170607165000'])
         self.assertIs(result, emptyLine, 'result must be emptyLine')
 
 
@@ -31,31 +32,34 @@ class TestLineFactory(unittest.TestCase):
 
         factory = LineFactory()
 
-        result = factory.build('130\tfoo')
+        result = factory.build('130\t20170607165000')
         self.assertIsInstance(result, Line130)
 
-        result = factory.build('140\tfoo')
+        result = factory.build('140\t20170607165000')
         self.assertIsInstance(result, Line140)
 
-        result = factory.build('150\tfoo')
+        result = factory.build('150\t20170607165000')
         self.assertEqual(result, emptyLine)
 
 
 class TestLinesFactory(unittest.TestCase):
 
     __DATA = \
-        '130\tfoo\n' \
-        '140\tfoo\n' \
-        '150\tfoo\n'
+        '130\t20170607165000\n' \
+        '140\t20170607165000\n' \
+        '150\t20170607165000\n'
             
     def test_lines_builder(self):
         factory = LinesFactory()
-        result = factory.build(self.__DATA)
-        self.assertEqual(len(result), 2)
+        results = factory.build(self.__DATA)
+        self.assertEqual(len(results), 2)
 
         with open("tests/test_line_builder.txt", "r") as file:
             data = file.read()
-            result = factory.build(data)
-            self.assertEqual(len(result), 22)
+            results = factory.build(data)
+            self.assertEqual(len(results), 22)
+
+            for l in results:
+                print(l)
 
 

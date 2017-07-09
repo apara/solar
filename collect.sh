6d56bc05694c9c038db9bf3a7059fc08e6713bb4
apara <apara@standardset.com>
2017-07-09 14:46:50 -0700

    Avoid starting tail at the end of collect

--- collect.sh
+++ collect.sh
@@ -1,6 +1,7 @@
 #!/bin/bash --debug
+cd /home/apara/project/solar
 pkill python
 rm collect.log
 touch collect.log
 nohup bash -c '. /home/apara/python-env/solar2/bin/activate;python main.py' > collect.log 2>&1 &
-tail -f collect.log
+# tail -f collect.log
